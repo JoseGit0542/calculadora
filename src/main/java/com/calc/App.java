24e72a69a597b7aac486357d4c07680469fe1be3
Jose <jose.xbox.chan@gmail.com>
2025-04-08 13:42:16 +0200

    Primera subida

---
+++ src/main/java/com/calc/App.java
@@ -0,0 +1,15 @@
+package com.calc;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Calculadora c1 = new Calculadora(2, 4);
+        System.out.println( "Hello World!" );
+        System.out.println(c1.suma());
+        
+    }
+}
